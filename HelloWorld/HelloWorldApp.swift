5adb71d7e2b57ab3529e80133a39a46a08600715
kishore Jr <36214175+kishoreravi24@users.noreply.github.com>
2021-04-10 18:25:50 +0530

    Initial commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Kishore Jr on 10/04/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
